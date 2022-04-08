4cfc69013cced5758f7555019c86430f09788ea4
Y888yDev <dony888y@gmail.com>
2022-04-08 15:34:20 +0200

    Updates

---
+++ precache-manifest.070c1f44cc7737d138b0540edc59ccdc.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "613fffd2f5ffc07ca1e28648d626fca0",
+    "url": "/kimbo/index.html"
+  },
+  {
+    "revision": "f1082b65ae2d420eb83a",
+    "url": "/kimbo/static/css/main.776e5cf6.chunk.css"
+  },
+  {
+    "revision": "eb3f2c92688a83552be2",
+    "url": "/kimbo/static/js/2.c7e04386.chunk.js"
+  },
+  {
+    "revision": "f1082b65ae2d420eb83a",
+    "url": "/kimbo/static/js/main.c8329a6f.chunk.js"
+  },
+  {
+    "revision": "9f50b38d8861e0d8192d",
+    "url": "/kimbo/static/js/runtime~main.a364c47c.js"
+  },
+  {
+    "revision": "01b58221b4998a363fe63a589734dbbc",
+    "url": "/kimbo/static/media/1.01b58221.webp"
+  },
+  {
+    "revision": "c9fae025f17754fc8ab31616c519e682",
+    "url": "/kimbo/static/media/1.c9fae025.jpeg"
+  },
+  {
+    "revision": "c51bbcf20ea0a31bd2ced2576055e24a",
+    "url": "/kimbo/static/media/2.c51bbcf2.webp"
+  },
+  {
+    "revision": "e97e8e2792583fb289eccd2b9482aa99",
+    "url": "/kimbo/static/media/2.e97e8e27.jpg"
+  },
+  {
+    "revision": "dba66bf8f53dcababe35462ef9badd41",
+    "url": "/kimbo/static/media/3.dba66bf8.webp"
+  },
+  {
+    "revision": "cefbc3e32beb1fac977719c5a72376ed",
+    "url": "/kimbo/static/media/4.cefbc3e3.webp"
+  },
+  {
+    "revision": "a093eee984faff662b4708e271d5bb3e",
+    "url": "/kimbo/static/media/6.a093eee9.webp"
+  },
+  {
+    "revision": "2fe9472755e7245157ac2655d1bd9afb",
+    "url": "/kimbo/static/media/7.2fe94727.webp"
+  },
+  {
+    "revision": "a068329efc312a596592506470bebe5f",
+    "url": "/kimbo/static/media/bg3.a068329e.jpg"
+  },
+  {
+    "revision": "6df65644b868d0a7d5287cdb3ff04926",
+    "url": "/kimbo/static/media/bgs2.6df65644.jpg"
+  },
+  {
+    "revision": "9b7a308c0c617c9553935bf437be0aea",
+    "url": "/kimbo/static/media/coffeeRoasted.9b7a308c.png"
+  },
+  {
+    "revision": "03269e2a9c73efddabdb907777db62b7",
+    "url": "/kimbo/static/media/logo.03269e2a.svg"
+  },
+  {
+    "revision": "03269e2a9c73efddabdb907777db62b7",
+    "url": "/kimbo/static/media/logolg.03269e2a.svg"
+  }
+]);
